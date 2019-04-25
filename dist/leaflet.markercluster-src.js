3824e97caf77a90841033737752107443bf57e7b
Bradley Wadsworth <bradley.wadsworth@cgi.com>
2019-04-25 17:15:02 +0100

    Add type definitions

--- dist/leaflet.markercluster-src.js
+++ dist/leaflet.markercluster-src.js
@@ -1,5 +1,5 @@
 /*
- * Leaflet.markercluster 1.4.1+master.93bf540,
+ * Leaflet.markercluster 1.4.2+master.0af03b6,
  * Provides Beautiful Animated Marker Clustering functionality for Leaflet, a JS library for interactive maps.
  * https://github.com/Leaflet/Leaflet.markercluster
  * (c) 2012-2017, Dave Leaver, smartrak
